8c4c6eb465edab62412a62e239493f073a46a4ae
syed aadam ahmad <98835147+syedaadamahmad@users.noreply.github.com>
2025-11-10 13:31:33 +0530

    modified the chat url path

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
  
 export function middleware(request) {
-  return NextResponse.redirect(new URL('/chat/flight', request.url))
+  return NextResponse.redirect(new URL('/chat', request.url))
 }
  
 export const config = {
